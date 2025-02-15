5e3968db0f61cdf6b919d0f4e6a7f13333f3f558
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-02-15 13:45:01 +0200

    Move base path logic to useEffect and useState

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 'use client'
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
   // This is really important. When deployed the path of all the images and other assets will change. If you do not add this in the front of URLs, the page will break.
-  const basePath = window.location.pathname.split('/')[1] || ''
-  const pathPrefix = basePath ? `/${basePath}` : ''
+  const [pathPrefix, setPathPrefix] = useState('');
+
+  useEffect(() => {
+    const basePath = window.location.pathname.split('/')[1] || '';
+    setPathPrefix(basePath ? `/${basePath}` : '');
+  }, [])
+
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
