75597406fc1491efb8453bb40a09d0868914e607
GreenBean0303 <lootsmannagnes@gmail.com>
2025-03-14 08:34:39 +0200

    client test subjects

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -1,9 +1,29 @@
-'use client'
+'use client';
+
+import React, { useState, useEffect } from "react";
 
 const ClientPage = () => {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    fetch('../data/client.json') 
+      .then(response => response.json())
+      .then(data => setClients(data))
+      .catch(error => console.error("Error loading client data:", error));
+  }, []);
 
   return (
-    <div>ClientPage</div>
-  )
-}
-export default ClientPage
+    <div>
+      <h1>Client Page</h1>
+      <ul>
+        {clients.map(client => (
+          <li key={client.id}>
+            {client.eesnimi} {client.perenimi} - Isikukood: {client.isikukood}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ClientPage;
